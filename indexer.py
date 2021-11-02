fcb9ec2bb77aa19f9e051918130c6859baa39cde
José Gonçalves <jpsextafeira@gmail.com>
2021-11-02 14:37:54 +0000

    More work on the SpimiIndexer (indexer.py)

--- indexer.py
+++ indexer.py
@@ -7,9 +7,19 @@ import spimi
 
 
 class SpimiIndexer:
-    def __init__(self, index_name: str):
+    def __init__(self, index_name: str, memory_threshold: float):
+        """
+        Creates a SpimiIndexer that can be called iteratively
+        on each document.
+        :param index_name: The name of the final merged index
+        :param memory_threshold: The amount of memory to be used as decimal between 0 and 1.
+        For example 0.5 would mean 50% of the memory available.
+        """
         self.index_name = index_name
         self.block_names: List[str] = []
+        self.write_block = spimi.block_writer("block")
+        self.postings_dictionary = PostingsDictionary()
+        self.memory_threshold = memory_threshold
 
     def add_review(self, review: ProcessedDocument):
         pass
