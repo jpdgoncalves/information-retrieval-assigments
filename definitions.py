ce1b02e32227dd848b82c961bf46905d37b88d91
José Gonçalves <jpsextafeira@gmail.com>
2021-12-05 17:03:33 +0000

    Moving all data classes and type definitions to definitions.py. It avoids repeating code when using type hints.

---
+++ definitions.py
@@ -0,0 +1,55 @@
+from typing import Iterable, DefaultDict, Tuple, List, Callable, TextIO, Dict
+
+from dataclasses import dataclass
+
+
+class CostumerReview:
+    def __init__(self, doc_id: int, review_id: str, product_title: str, review_headline: str, review_body: str):
+        self.doc_id = doc_id
+        self.review_id = review_id
+        self.product_title = product_title
+        self.review_headline = review_headline
+        self.review_body = review_body
+
+    @property
+    def content(self):
+        return f"{self.product_title} {self.review_headline} {self.review_body}"
+
+
+class Token:
+    def __init__(self, word: str, pos: int):
+        self.word = word
+        self.pos = pos
+
+
+class ProcessedDocument:
+    def __init__(self, _id: int, review_id: str, tokens: Iterable[Token], document_length: int):
+        self.id = _id
+        self.review_id = review_id
+        self.tokens = tokens
+        self.document_length = document_length
+
+
+@dataclass
+class IndexingStatistics:
+    indexing_time: float
+    index_size_on_disk: int
+    term_count: int
+    blocks_used: int
+
+
+ReviewId = str
+DocId = int
+Weight = float
+Position = int
+DocLength = int
+Term = str
+
+Posting = Tuple[Weight, List[Position]]
+Postings = Dict[DocId, Posting]
+Vocabulary = DefaultDict[Term, Postings]
+TermIndex = Dict[Term, List[Position]]
+
+Processor = Callable[[Iterable[str]], Iterable[str]]
+WeightFunction = Callable[[TermIndex], Dict[Term, Posting]]
+BlockVocabularyFormatter = Callable[[TextIO, Vocabulary], None]
