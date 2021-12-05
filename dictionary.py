9bf52c7df4fb188cd451a1a720a55c121f4c57a1
José Gonçalves <jpsextafeira@gmail.com>
2021-12-05 17:04:27 +0000

    Refactoring dictionary so each posting contains a pair with a score and a list of positions. The score functions can be found in this module.

--- dictionary.py
+++ dictionary.py
@@ -2,26 +2,68 @@
 Module containing a simple helper Data Structure that handles the construction
 of an inverted dictionary.
 """
-from typing import DefaultDict, Dict, List
+from typing import List
+from definitions import (
+    ProcessedDocument,
+    ReviewId, Vocabulary, Postings,
+    WeightFunction, TermIndex
+)
 
 from collections import defaultdict
 
-from processor import ProcessedDocument
-
-Postings = DefaultDict[int, List[int]]
+import math
 
 
-def postings():
-    return defaultdict(list)
+def _postings() -> Postings:
+    return {}
+
+
+def _aggregate(document: ProcessedDocument) -> TermIndex:
+    postings = defaultdict(list)
+
+    for token in document.tokens:
+        postings[token.word].append(token.pos)
+
+    return postings
+
+
+# noinspection PyTypeChecker
+def normalized_tf_weight_function(term_index: TermIndex) -> Postings:
+    postings = {}
+
+    for term, positions in term_index.items():
+        postings[term] = 1 + math.log10(len(positions))
+
+    total_squared_score = sum(score ** 2 for score in postings.values())
+    score_normalizer = math.sqrt(total_squared_score)
+
+    for term, positions in term_index.items():
+        postings[term] = (postings[term] / score_normalizer, positions)
+
+    return postings
+
+
+# noinspection PyTypeChecker
+def term_count_function(term_index: TermIndex) -> Postings:
+    postings = {}
+
+    for term, positions in term_index.items():
+        postings[term] = (len(positions), positions)
+
+    return postings
 
 
 class PostingsDictionary:
-    def __init__(self):
-        self.doc_id_mapping: Dict[int, str] = {}
-        self.postings_list: DefaultDict[str, Postings] = defaultdict(postings)
+    def __init__(self, weight_function: WeightFunction):
+        self.review_ids: List[ReviewId] = []
+        self.postings_list: Vocabulary = defaultdict(_postings)
+        self.weight_function = weight_function
 
     def add_document(self, document: ProcessedDocument):
-        self.doc_id_mapping[document.id] = document.review_id
+        term_index = _aggregate(document)
+        postings = self.weight_function(term_index)
 
-        for token in document.tokens:
-            self.postings_list[token.word][document.id].append(token.pos)
+        self.review_ids.append(document.review_id)
+
+        for term, posting in postings.items():
+            self.postings_list[term][document.id] = posting
