d0607939dd455df7c85e1e26764d0516e208db19
José Gonçalves <jpsextafeira@gmail.com>
2021-12-23 10:33:11 +0000

    Moving some post-processing like calculating weights into the processor.py module.

--- dictionary.py
+++ dictionary.py
@@ -2,77 +2,27 @@
 Module containing a simple helper Data Structure that handles the construction
 of an inverted dictionary.
 """
-from typing import List, Iterable
+from typing import List
 from definitions import (
-    ProcessedReview, Token,
+    ProcessedReview,
     ReviewId, Vocabulary, Postings,
-    WeightFunction, TermIndex, TermPostings
 )
 
 from collections import defaultdict
-
-import math
 
 
 def _postings() -> Postings:
     return []
 
 
-def _aggregate(tokens: Iterable[Token]) -> TermIndex:
-    postings = defaultdict(list)
-
-    for word, pos in tokens:
-        postings[word].append(pos)
-
-    return postings
-
-
-# noinspection PyTypeChecker
-def normalized_tf_weight_function(term_index: TermIndex) -> TermPostings:
-    postings = {}
-
-    for term, positions in term_index.items():
-        postings[term] = 1 + math.log10(len(positions))
-
-    total_squared_score = sum(score ** 2 for score in postings.values())
-    score_normalizer = math.sqrt(total_squared_score)
-
-    for term, positions in term_index.items():
-        postings[term] = (postings[term] / score_normalizer, positions)
-
-    return postings
-
-
-# noinspection PyTypeChecker
-def term_count_function(term_index: TermIndex) -> TermPostings:
-    postings = {}
-
-    for term, positions in term_index.items():
-        postings[term] = (len(positions), positions)
-
-    return postings
-
-
 class PostingsDictionary:
-    def __init__(self, weight_function: WeightFunction):
+    def __init__(self):
         self.review_ids: List[ReviewId] = []
         self.postings_list: Vocabulary = defaultdict(_postings)
-        self.weight_function = weight_function
 
     def add_document(self, review: ProcessedReview):
-        doc_id, review_id, tokens, _ = review
-        term_index = _aggregate(tokens)
-        postings = self.weight_function(term_index)
-
+        doc_id, review_id, postings, _ = review
         self.review_ids.append(review_id)
 
         for term, posting in postings.items():
             self.postings_list[term].append((doc_id, posting))
-
-
-def tf_idf_dictionary():
-    return PostingsDictionary(normalized_tf_weight_function)
-
-
-def term_count_dictionary():
-    return PostingsDictionary(term_count_function)
--- processor.py
+++ processor.py
@@ -1,16 +1,52 @@
 """
 Module containing the DocumentProcessor.
 """
+import math
 import Stemmer
-from typing import Set, Callable
+from collections import defaultdict
+from typing import Set, Callable, List
 
-from definitions import RawReview, ProcessedReview
+from definitions import (
+    RawReview, ProcessedReview, ProcessedQuery,
+    TermIndex, TermPostings, WeightFunction
+)
 
 import re
 
 
 _regex_pattern = re.compile("[^a-z]")
 _stemmer = Stemmer.Stemmer("english")
+
+
+def query_processor(
+        min_token_len: int,
+        stopwords: Set[str],
+        stemmer: Callable[[str], str]
+):
+    def process_query(query: str) -> ProcessedQuery:
+        words = process_str(query, min_token_len, stopwords, stemmer)
+        query_len = len(words)
+        return query_len, aggregate(words)
+
+    return process_query
+
+
+def review_processor(
+        min_token_len: int,
+        stopwords: Set[str],
+        stemmer: Callable[[str], str],
+        weight_function: WeightFunction
+):
+    def process_review(raw_review: RawReview) -> ProcessedReview:
+        doc_id, review_id, content = raw_review
+
+        words = process_str(content, min_token_len, stopwords, stemmer)
+        postings = weight_function(aggregate(words))
+        doc_length = len(words)
+
+        return doc_id, review_id, postings, doc_length
+
+    return process_review
 
 
 def english_stemmer(word: str):
@@ -21,25 +57,45 @@ def no_stemmer(word: str):
     return word
 
 
-def review_processor(
+def process_str(
+        content: str,
         min_token_len: int,
         stopwords: Set[str],
         stemmer: Callable[[str], str],
 ):
-    def process_review(raw_review: RawReview) -> ProcessedReview:
-        doc_id, review_id, content = raw_review
+    words = _regex_pattern.sub(" ", content.lower()).split(" ")
+    return [stemmer(word) for word in words if min_token_len <= len(word) < 50 and word not in stopwords]
 
-        words = _regex_pattern.sub(" ", content.lower()).split(" ")
-        words = [stemmer(word) for word in words if min_token_len <= len(word) < 50 and word not in stopwords]
 
-        tokens = [(word, pos) for pos, word in enumerate(words)]
-        doc_length = len(tokens)
+def aggregate(words: List[str]) -> TermIndex:
+    count_dict = defaultdict(list)
 
-        return (
-            doc_id,
-            review_id,
-            tokens,
-            doc_length
-        )
+    for position, word in enumerate(words):
+        count_dict[word].append(position)
 
-    return process_review
+    return count_dict
+
+
+# noinspection PyTypeChecker
+def normalized_tf_idf_term_index(term_index: TermIndex) -> TermPostings:
+    postings = {}
+
+    for term, positions in term_index.items():
+        postings[term] = 1 + math.log10(len(positions))
+
+    total_squared_score = sum(score ** 2 for score in postings.values())
+    score_normalizer = math.sqrt(total_squared_score)
+
+    for term, positions in term_index.items():
+        postings[term] = (postings[term] / score_normalizer, positions)
+
+    return postings
+
+
+def count_term_index(term_index: TermIndex) -> TermPostings:
+    postings = {}
+
+    for term, positions in term_index.items():
+        postings[term] = (len(positions), positions)
+
+    return postings
